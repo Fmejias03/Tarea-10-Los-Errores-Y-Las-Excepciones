7a12ea5a2633f034244c74e26a797480c5673b99
Fmeji <Fmeji@hotmail.es>
2023-03-23 11:35:44 +0100

    Ejercicio 2 apartado A

---
+++ src/main/java/Ejercicio_2/Ejercicio_2.java
@@ -0,0 +1,20 @@
+package Ejercicio_2;
+
+public class Ejercicio_2 {
+    /*El objetivo es crear una mini calculadora que realice sumas, restas, multiplicaciones, divisiones enteras y con resto.
+
+Esta calculadora maneja enteros de tipo int. Con el tipo int, los valores deben estar entre -2.147.483.648 y 2.147.483.647. Si una variable de tipo int se inicializa a 2.000.000.000 y la variable se multiplica por dos, la variable contendrá: 294.967.296. Ha habido un "desbordamiento". Desafortunadamente, esto no causa un error ni una excepción.
+
+Otro problema con las operaciones con enteros, es que la división por cero no está definida matemáticamente. Cuando se realiza una operación de este tipo, se genera una excepción de tipo ArithmeticException.
+
+La calculadora debe tener esto en cuenta para evitar mostrar un resultado falso.
+
+a. Creación de una clase DesbordaCapacidadExcepcion*/
+
+}
+
+public class DesbordaCapacidadExcepcion{
+    public DesbordaCapacidadExcepcion(){
+        System.out.println("El resultado desborda la capacidad de esta calculadora");
+    }
+}
