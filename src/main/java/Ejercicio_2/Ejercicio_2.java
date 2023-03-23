d91857ce370914557bb69dfd85f738feb555202b
Fmeji <Fmeji@hotmail.es>
2023-03-23 11:45:15 +0100

    Ejercicio 2 apartado B mitad

--- src/main/java/Ejercicio_2/Ejercicio_2.java
+++ src/main/java/Ejercicio_2/Ejercicio_2.java
@@ -9,8 +9,38 @@ Otro problema con las operaciones con enteros, es que la división por cero no e
 
 La calculadora debe tener esto en cuenta para evitar mostrar un resultado falso.
 
-a. Creación de una clase DesbordaCapacidadExcepcion*/
+a. Creación de una clase DesbordaCapacidadExcepcion
+b. Creación de la clase de utilidad Operacion
+Esta clase contiene tres métodos para sumar, restar y multiplicar (no es necesario crear un método dividir, ya que la división de números enteros no se puede desbordar). Todos toman enteros como argumentos y devuelven un entero.
+
+Los tres generan excepciones del tipo DesbordaCapacidadException, en caso de que el cálculo provoque un desbordamiento de la capacidad de un entero de tipo int.
+
+Una manera fácil de probar si un cálculo con tipos int excede su capacidad, es realizar el cálculo con tipos long y verificar si los resultados son diferentes.*/
 
 }
+public class Operacion{
+    public static int sumar(int a, int b) throws DesbordaCapacidadExcepcion {
+        long resultado = (long) a + (long) b;
+        if (resultado > Integer.MAX_VALUE || resultado < Integer.MIN_VALUE) {
+            throw new DesbordaCapacidadExcepcion("El resultado de la suma excede la capacidad de un entero");
+        }
+        return (int) resultado;
+    }
 
+    public static int restar(int a, int b) throws DesbordaCapacidadExcepcion {
+        long resultado = (long) a - (long) b;
+        if (resultado > Integer.MAX_VALUE || resultado < Integer.MIN_VALUE) {
+            throw new DesbordaCapacidadExcepcion("El resultado de la resta excede la capacidad de un entero");
+        }
+        return (int) resultado;
+    }
+
+    public static int multiplicar(int a, int b) throws DesbordaCapacidadExcepcion {
+        long resultado = (long) a * (long) b;
+        if (resultado > Integer.MAX_VALUE || resultado < Integer.MIN_VALUE) {
+            throw new DesbordaCapacidadExcepcion("El resultado de la multiplicación excede la capacidad de un entero");
+        }
+        return (int) resultado;
+    }
+}
 
